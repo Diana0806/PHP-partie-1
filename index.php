26e754a9fd156cbb44d69f7aa33bfb1b951f80c9
Diana0806 <diananovak871@gmail.com>
2023-06-27 16:54:02 +0200

    add PHP

---
+++ index.php
@@ -0,0 +1,70 @@
+<!-- Ex 1 -->
+
+<?php
+$name = 'Mathieu';
+echo $name;
+?>
+<br>
+
+<!-- Ex 2 -->
+<?php
+$lastname = 'Green';
+$firstname = 'Mathieu';
+$age = 23;
+echo $lastname . " " . $firstname . " " . $age;
+?>
+<br>
+
+<!-- Ex 3 -->
+<?php
+$km = 1;
+echo $km;
+$km = 3;
+echo $km;
+$km = 125;
+echo $km;
+?>
+<br>
+
+<!-- Ex 4 -->
+<?php
+$stringVar = 'Hello';
+$intVar = 23;
+$floatVar = 3.1;
+$boolVar = true;
+
+echo $stringVar . " " . $intVar . " " . $floatVar . " " . $boolVar;
+?>
+<br>
+
+<!-- Ex 5 -->
+<?php
+$noValue = NULL;
+?>
+
+<!-- Ex 6 -->
+<?php
+$name = "Mathieu";
+echo "Bonjour $name , comment vas tu ?";
+?>
+<br>
+
+<!-- Ex 7 -->
+<?php
+$lastname = 'Green';
+$firstname = 'Mathieu';
+$age = 23;
+echo 'Bonjour ' . $lastname . " " . $firstname .', tu as ' . $age .' ans.';
+?>
+<br>
+
+<!-- Ex 8 -->
+<?php
+$var1 = 3 + 4;
+echo $var1 . "<br>";
+$var2 = 5 * 20;
+echo $var2 . "<br>";
+$var3 = 45 / 5;
+echo $var3;
+?>
+<br>
